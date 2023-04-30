244049042ad6d7dc39ec092ec33b5c5c9506fb6b
vadim <105228689+guessiwillbefine@users.noreply.github.com>
2023-04-30 19:06:19 +0300

    profile

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,10 +1,14 @@
+import {AccountStatus} from "./AccountStatus";
+import {Role} from "./Role";
+
 export interface User {
   id: number,
   username: string
   firstName: string;
   lastName: string;
   email: string;
-  role: string;
-  status: string;
+  role: Role;
+  status: AccountStatus;
   factoryId: number;
 }
+
